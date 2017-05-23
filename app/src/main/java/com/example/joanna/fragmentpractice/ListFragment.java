171ad1772b1a26c960c699d6b1545c92aa68b65b
Joannalq <joanna851026@hotmail.com>
2017-05-23 20:05:55 +1000

    add new view page and back stack

--- app/src/main/java/com/example/joanna/fragmentpractice/ListFragment.java
+++ app/src/main/java/com/example/joanna/fragmentpractice/ListFragment.java
@@ -1,9 +1,9 @@
 package com.example.joanna.fragmentpractice;
 
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -11,7 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ListFragment extends LoggingFragment {
+public class ListFragment extends Fragment {
     public interface  OnSelectedInterface{
         void onListSelected(int index);
     }
@@ -19,7 +19,7 @@ public class ListFragment extends LoggingFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        Log.d(LoggingFragment.TAG,"onCreateView");
+
         OnSelectedInterface listener= (OnSelectedInterface) getActivity();
         View view=inflater.inflate(R.layout.fragment_list,container,false);
         RecyclerView recyclerView= (RecyclerView) view.findViewById(R.id.listRecycleView);
--- app/src/main/java/com/example/joanna/fragmentpractice/MainActivity.java
+++ app/src/main/java/com/example/joanna/fragmentpractice/MainActivity.java
@@ -1,22 +1,25 @@
 package com.example.joanna.fragmentpractice;
 
 import android.app.*;
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.*;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class MainActivity extends LoggingActivity implements ListFragment.OnSelectedInterface {
+public class MainActivity extends AppCompatActivity implements ListFragment.OnSelectedInterface {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment savedFrag= (ListFragment) getFragmentManager().findFragmentById(R.id.placeHolder);
+        ListFragment savedFrag= (ListFragment) getSupportFragmentManager().findFragmentById(R.id.placeHolder);
         if(savedFrag==null) {
             ListFragment fragment = new ListFragment();
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.add(R.id.placeHolder, fragment);
             fragmentTransaction.commit();
         }
@@ -25,5 +28,11 @@ public class MainActivity extends LoggingActivity implements ListFragment.OnSele
     @Override
     public void onListSelected(int index) {
         Toast.makeText(MainActivity.this,Recipes.names[index],Toast.LENGTH_SHORT).show();
+        ViewPagerFragment fragment = new ViewPagerFragment();
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.placeHolder, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
