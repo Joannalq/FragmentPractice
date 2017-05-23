f41a766aaac832376cad867faf1f9b7cfe87f5bb
Joannalq <joanna851026@hotmail.com>
2017-05-23 20:33:01 +1000

    initialize fragment data

--- app/src/main/java/com/example/joanna/fragmentpractice/MainActivity.java
+++ app/src/main/java/com/example/joanna/fragmentpractice/MainActivity.java
@@ -9,18 +9,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.OnSelectedInterface {
-
+    public static final String LIST="list_fragment";
+    public static final String VIEW="view_fragment";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment savedFrag= (ListFragment) getSupportFragmentManager().findFragmentById(R.id.placeHolder);
+        //ListFragment savedFrag= (ListFragment) getSupportFragmentManager().findFragmentById(R.id.placeHolder);
+        ListFragment savedFrag= (ListFragment) getSupportFragmentManager().findFragmentByTag(LIST);
         if(savedFrag==null) {
             ListFragment fragment = new ListFragment();
             android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
             android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.placeHolder, fragment);
+            //fragmentTransaction.add(R.id.placeHolder, fragment);
+            fragmentTransaction.add(R.id.placeHolder, fragment,LIST);
             fragmentTransaction.commit();
         }
     }
@@ -29,9 +32,13 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnSe
     public void onListSelected(int index) {
         Toast.makeText(MainActivity.this,Recipes.names[index],Toast.LENGTH_SHORT).show();
         ViewPagerFragment fragment = new ViewPagerFragment();
+        Bundle bundle=new Bundle();
+        bundle.putInt(ViewPagerFragment.KEY_INDEX,index);
+        fragment.setArguments(bundle);
         android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.placeHolder, fragment);
+        //fragmentTransaction.replace(R.id.placeHolder, fragment);
+        fragmentTransaction.replace(R.id.placeHolder, fragment,VIEW);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
